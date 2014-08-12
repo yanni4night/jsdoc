7651ab25f6742c4c33730577174339914b2bae95
yinyong <yinyong@sogou-inc.com>
2014-08-12 18:22:00 +0800

    touch interface index.js

---
+++ index.js
@@ -0,0 +1,12 @@
+/**
+  * Copyright (C) 2014 yanni4night.com
+  * index.js
+  *
+  * changelog
+  * 2014-08-12[18:20:59]:authorized
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+module.exports = require('./src/jsdoc.js');
---
+++ src/jsdoc.js
@@ -0,0 +1,12 @@
+/**
+  * Copyright (C) 2014 yanni4night.com
+  * jsdoc.js
+  *
+  * changelog
+  * 2014-08-12[18:21:32]:authorized
+  *
+  * @author yanni4night@gmail.com
+  * @version 0.1.0
+  * @since 0.1.0
+  */
+module.exports = {};
